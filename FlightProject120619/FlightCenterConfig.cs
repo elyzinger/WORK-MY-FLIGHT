5648be2867c49379e28b125516a4cf98b02e803c
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-09-02 09:56:31 +0300

    Add files via upload

---
+++ FlightProject120619/FlightCenterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightProject120619
+{
+    public static class FlightCenterConfig
+    {
+
+        public const string ADMIN_NAME = "ADMIN";
+        public const string ADMIN_PASSWORD = "9999";
+        public const string DAO_CON = @"Data Source=.;Initial Catalog=WorkMyFlights;Integrated Security=True";
+        public const int historyTimer = 1000 * 60 * 60 * 24;
+    }
+}
